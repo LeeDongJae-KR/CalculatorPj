9fb344927028580ab37a4c49df64cf6c4e22b4fb
LeeDongJae-KR <movementj98@gmail.com>
2025-07-14 13:34:54 +0900

    ✨feat: 저장된 결과값 출력 및 삭제 기능 추가

--- src/main/java/org/sparta/calculator/v2/App2.java
+++ src/main/java/org/sparta/calculator/v2/App2.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class App2 {
     public static void main(String[] args) {
 
+        // 사칙 연산에 사용될 변수 초기화
         int num1 = 0;
         int num2 = 0;
         int result = 0;
         char operator = ' ';
         String exit = "";
+
+        // 사칙 연산에 사용될 객체 선언
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
         CheckValue checkValue = new CheckValue();
 
+        // 압력값 검증 및 사칙연산 수행
         do {
             num1 = checkValue.checkNum(scanner,"첫 번째 숫자를 입력해라! : ");
             num2 = checkValue.checkNum(scanner, "두 번째 숫자를 입력해라! : ");
@@ -26,11 +30,16 @@ public class App2 {
                 System.out.println(e.getMessage());
             }
 
-            System.out.print("'eixt'을 입력하면 나가게 해줄게 ~");
+            // 계산된 결과값 출력 및 처음값 삭제
+            calculator.printResults();
+            calculator.deleteResult(scanner, "맨 첫번째 결과값을 삭제 하려면 Yes 라고 입력해줘 ~");
+            calculator.printResults();
+
+            System.out.print("'Eixt'을 입력하면 나가게 해줄게 ~");
 
             exit = scanner.nextLine();
 
-        } while (!exit.equals("exit"));
+        } while (!exit.equalsIgnoreCase("exit"));
         scanner.close();
         System.out.println("시스템 종료 -_-");
     }
--- src/main/java/org/sparta/calculator/v2/Calculator.java
+++ src/main/java/org/sparta/calculator/v2/Calculator.java
@@ -5,13 +5,14 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 @Getter
 @Setter
 public class Calculator {
 
     // 연산 결과를 저장하는 컬렉션 타입의 필드
-    private List<Integer> result = new ArrayList<>();
+    List<Integer> result = new ArrayList<>();
 
     // 양의 정수 2개와 연산 기호를 매개변수 받아 사칙 연산 기능을 수행하는 메서드
     public int calculate(int num1, int num2, char operator) throws RuntimeException {
@@ -32,10 +33,39 @@ public class Calculator {
                 if (num2 == 0) throw new ArithmeticException("0으로 나눌 수 없어요");
                 value = num1 / num2;
                 break;
-                default:
-                    throw new IllegalArgumentException("지원하지 않는 연산자에요");
+            default:
+                throw new IllegalArgumentException("지원하지 않는 연산자에요");
         }
         result.add(value);
         return value;
+    }
+
+    // 리스트에 저장된 첫번째 값 삭제 메서드
+    public boolean deleteResult(Scanner scanner, String pompt) {
+        System.out.print(pompt);
+        String input = scanner.nextLine().trim();
+
+        if (input.equalsIgnoreCase("Yes")) {
+            if (!result.isEmpty()) {
+                int deleted = result.remove(0);
+                System.out.println("첫 번째 결과(" + deleted + ") 를 삭제 했다잇!");
+            } else {
+                System.out.println("삭제할게 없는데? ");
+            }
+            return true;
+        }
+        return false;
+    }
+
+    // 리스트에 저장된 값 정리해서 출력
+    public void printResults() {
+        if (!result.isEmpty()) {
+            System.out.println("저장된 결과 리스트");
+            for (int i = 0; i < result.size(); i++) {
+                System.out.println((i + 1) + "번째 : " + result.get(i));
+            }
+        } else {
+            System.out.println("저장된 결과가 없는데?");
+        }
     }
 }
